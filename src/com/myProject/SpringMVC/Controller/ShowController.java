1c7eb0f4d66e72ce0c29c73fc0f32bced3ec04f5
a1655909198 <575070804@qq.com>
2014-05-25 13:58:05 +0800

    修改

--- src/com/myProject/SpringMVC/Controller/ShowController.java
+++ src/com/myProject/SpringMVC/Controller/ShowController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.portlet.bind.annotation.RenderMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
