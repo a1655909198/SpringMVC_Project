a4bfb983f3110de031445d7f791a11acd44fb0b2
a1655909198 <575070804@qq.com>
2014-05-25 13:39:12 +0800

    添加功能

---
+++ src/com/myProject/SpringMVC/Controller/ShowController.java
@@ -0,0 +1,30 @@
+package com.myProject.SpringMVC.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.portlet.bind.annotation.RenderMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ShowController {
+	/**
+	 * ึ๗าณ
+	 * @author tianlei
+	 * @since 2014.05.25
+	 * @param
+	 * @param
+	 * @return
+	 * */
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	public ModelAndView index(HttpServletRequest request,HttpServletResponse response){
+		ModelMap map = new ModelMap();
+		String str="android";
+		map.addAttribute("name",str);
+		return new ModelAndView("index",map);
+	}
+}
